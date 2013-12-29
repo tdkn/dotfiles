e2dd2f728eb0366d22d032e5d8e8d7b0eaa3ceee
tdkn <web@tedokon.com>
2013-12-29 21:16:23 +0900

    Rsync: remove the unnecessary backslash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "home directory -> $HOME"
+echo ""
 echo "init dotfiles ..."
 echo ""
 
@@ -9,14 +10,18 @@ rsync -av --no-perms \
       --exclude "README.md" \
       --exclude "bootstrap.sh" . $HOME
 
+echo ""
 echo "init vim ..."
+echo ""
 
 # VIM BUNDLE PATH
 VBP="$HOME/.vim/bundle"
 
 [ ! -d $VBP ] && mkdir -p $VBP && \
   git clone github:Shougo/neobundle.vim $VBP/neobundle.vim && \
-  source $VBP/neobundle.vim/bin/neoinstall \
+  source $VBP/neobundle.vim/bin/neoinstall
 
 echo ""
 echo "complete!"
+echo ""
+
