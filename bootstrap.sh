b21f0c7dbf47ea99ec1adb2704603cb9438dd828
tdkn <web@tedokon.com>
2014-03-20 04:23:09 +0000

    Fix too deep symlink loop and cosmetic change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 DOTFILES_ROOT="`pwd`"
-
 DOTFILES=`ls -A --ignore='[^.]*' --ignore='.git'`
-
-TIME=`date +'~%Y-%m-%d-%H:%M:%S'`
-
-BUNDLER="$HOME/.vim/bundle"
+SUFFIX=`date +'~%Y-%m-%d-%H:%M:%S'`
+BUNDLER=~/.vim/bundle
 
 info () {
   printf "\n$1\n\n"
@@ -14,12 +11,12 @@ info () {
 
 info 'Checking directory ...'
 
-# Create directory `$HOME/.dotfiles' if not exist
+# Create directory `~/.dotfiles' if not exist
 if [ ! -d ~/.dotfiles ]; then
   echo "NG, directory '~/.dotfiles' not found"
   echo "Create symlink : ~/.dotfiles"
   echo -e "\t-> $DOTFILES_ROOT"
-  ln -s . ~/.dotfiles
+  ln -s $DOTFILES_ROOT ~/.dotfiles
 else
   echo "OK, directory '~/.dotfiles' found"
 fi
@@ -27,12 +24,12 @@ fi
 info 'Symlinking dotfiles ...'
 
 # Create symlinks
-cd $HOME
+cd
 for file in $DOTFILES
 do
   echo "Create symlink : ~/$file"
   echo -e "\t-> ~/.dotfiles/$file"
-  ln -sb --suffix=$TIME .dotfiles/$file ~/$file
+  ln -sb --suffix=$SUFFIX .dotfiles/$file ~/$file
 done
 
 info 'Installing Vim plugin manager ...'
@@ -50,6 +47,6 @@ info 'Complete!'
 
 unset DOTFILES_ROOT
 unset DOTFILES
-unset TIME
+unset SUFFIX
 unset BUNDLER
 
