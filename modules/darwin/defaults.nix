10135c019071544864be1782061f58bba5a094c7
Shun Tedokon <shun@tedokon.com>
2026-06-02 11:43:48 +0900

    Configure Darwin system defaults

--- modules/darwin/defaults.nix
+++ modules/darwin/defaults.nix
@@ -4,7 +4,28 @@
   # and application defaults here as the workstation preferences grow.
   system.defaults = {
     finder = {
+      AppleShowAllExtensions = true;
       FXPreferredViewStyle = "Nlsv";
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      _FXSortFoldersFirst = true;
+    };
+    dock = {
+      autohide = true;
+      mru-spaces = false;
+      showhidden = true;
+    };
+    ".GlobalPreferences" = {
+      "com.apple.mouse.scaling" = 2.0;
+    };
+    trackpad = {
+      Clicking = true;
+      TrackpadRightClick = true;
+      TrackpadThreeFingerDrag = true;
+    };
+    universalaccess = {
+      closeViewScrollWheelToggle = true;
+      mouseDriverCursorSize = 2.0;
     };
   };
 }
